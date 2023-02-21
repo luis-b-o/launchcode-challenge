af49cb23d07aa0426337f5d013ef6501c971ec93
Luis Oliveira <luis.oliveira@startse.com>
2023-02-21 19:16:18 -0300

    basic config of mui and nextjs

---
+++ config/theme.ts
@@ -0,0 +1,39 @@
+import { red } from "@mui/material/colors";
+import { createTheme } from "@mui/material/styles";
+import { Roboto } from "@next/font/google";
+
+export const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  fallback: ["Helvetica", "Arial", "sans-serif"],
+});
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#5F6CAF",
+    },
+    secondary: {
+      main: "#5BBFBA",
+    },
+    grey: {
+      100: "#f5f5f5",
+      200: "#eeeeee",
+      300: "#e0e0e0",
+      400: "#bdbdbd",
+      500: "#9e9e9e",
+      600: "#757575",
+      700: "#616161",
+      800: "#424242",
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
+});
+
+export default theme;
